a4466bedbaebd0492adb34332ddae98d4656807e
jimloxc9912 <lj5072354@gmail.com>
2025-07-15 20:29:48 -0600

    contexto global ChatContext

---
+++ src/context/ChatContext.jsx
@@ -0,0 +1,36 @@
+import { createContext, useReducer } from 'react'
+
+// 1. Crear el contexto global
+export const ChatContext = createContext()
+
+const initialState = {
+  messages: []
+}
+
+const chatReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_MESSAGE':
+      console.log('agregando mensaje...')
+      console.log(state)
+      return { ...state, messages: [...state.messages, action.payload] }
+    case 'SET_LOADING':
+      return { ...state, loading: action.payload }
+    default:
+      return state
+  }
+}
+
+/* dispatch({ type: 'ADD_MESSAGE', payload: { from: 'user', text: userPrompt } })
+dispatch({ type: 'ADD_MESSAGE', payload: { from: 'bot', text: res.data.response } }) */
+
+// 2. Provider
+
+export const ChatProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(chatReducer, initialState)
+
+  return (
+    <ChatContext.Provider value={{ state, dispatch }}>
+      {children}
+    </ChatContext.Provider>
+  )
+}
