6085e95581eb0ecb17d2bd824d2fed3f910f1545
jimloxc9912 <lj5072354@gmail.com>
2025-07-15 20:21:28 -0600

    chat finalizado en App

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import './index.css'
 import * as yup from 'yup'
 import axios from 'axios'
-import { useState } from 'react'
-import { useData } from './hooks/useData'
+import { useReducer, useState } from 'react'
 
 const schema = yup.object({
   userInput: yup
@@ -13,6 +12,21 @@ const schema = yup.object({
     .required('El mensaje es obligatorio')
 })
 
+const initialState = {
+  messages: []
+}
+
+const chatReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_MESSAGE':
+      console.log('agregando mensaje...')
+      console.log(state)
+      return { ...state, messages: [...state.messages, action.payload] }
+    default:
+      return state
+  }
+}
+
 export const App = () => {
   const { register, handleSubmit, formState: { errors } } = useForm({
     resolver: yupResolver(schema)
@@ -20,9 +34,7 @@ export const App = () => {
   // Guarda la respuesta de llama2
   const [response, setResponse] = useState('')
   const [loading, setLoading] = useState(false)
-  const { multiplicar } = useData()
-
-  console.log(multiplicar(5))
+  const [state, dispatch] = useReducer(chatReducer, initialState)
 
   const handlePregunta = async (data) => {
     console.log(data)
@@ -34,6 +46,9 @@ export const App = () => {
         stream: false
       })
       setResponse(res.data.response)
+      // Dispatch para guardar el mensaje del usuario
+      dispatch({ type: 'ADD_MESSAGE', payload: { from: 'user', text: data.userInput } })
+      dispatch({ type: 'ADD_MESSAGE', payload: { from: 'bot', text: res.data.response } })
     } catch (error) {
       console.error('error: ', error)
     } finally {
@@ -55,8 +70,16 @@ export const App = () => {
         >Preguntar
         </button>
       </form>
-      <div>
+      {/* <div>
         <p>{loading ? 'Generando respuesta 🚀' : response}</p>
+      </div> */}
+      <div>
+        {state.messages.map((msg, index) => (
+          <p key={index}>
+            <strong>{msg.from === 'user' ? 'Tú' : 'Bot'}:</strong>
+            {msg.text}
+          </p>
+        ))}
       </div>
     </>
   )
