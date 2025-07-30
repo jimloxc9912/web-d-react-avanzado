ac9618887fab9c0afebcdaecd5c42bc026483d32
jimloxc9912 <lj5072354@gmail.com>
2025-07-29 20:49:17 -0600

    mostrar mensajes en chat desde backend

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,17 @@
 import express from 'express'
 import cors from 'cors'
 import { generateFromOllama } from './ollamaService.js'
-import './db.js'
+import db from './db.js'
 
 const app = express()
 const PORT = 3001
 
 // Middleware para CORS y JSON
-
 app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Hola Mundo')
+  res.send('Hola mundo')
 })
 
 app.post('/api/chat', async (req, res) => {
@@ -23,10 +22,38 @@ app.post('/api/chat', async (req, res) => {
     res.json({ response })
   } catch (error) {
     console.error(error)
-    res.status(500).json({ error: 'Error al generar respuesta' })
+    res.status(500).json({ error: 'Error procesando la solicitud' })
   }
 })
 
+// GET: obtener mensajes
+app.get('/api/messages', async (req, res) => {
+  await db.read()
+  res.json(db.data.messages)
+})
+
+// POST: Ruta para agregar nuevo mensaje
+app.post('/api/messages', async (req, res) => {
+  // text, sender
+  const { text, sender } = req.body
+  if (!text || !sender) {
+    return res.status(400).json({ error: 'Faltan campos en el objeto' })
+  }
+
+  const newMessage = {
+    id: Date.now(),
+    text,
+    sender,
+    timestamp: new Date().toISOString()
+  }
+
+  await db.read()
+  db.data.messages.push(newMessage)
+  await db.write()
+
+  res.status(201).json(newMessage)
+})
+
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`)
+  console.log(`Servidor corriendo en: http://localhost:${PORT}`)
 })
